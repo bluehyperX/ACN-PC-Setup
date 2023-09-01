ed7bd88841ee0e117204a53e86ff0a9eba23fa2c
Sparsh Kumar <34296252+bluehypergiant@users.noreply.github.com>
2023-09-01 16:57:14 +0530

    Add files via upload

---
+++ upgrade.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
+sudo apt-get update -y
+echo -e $TEXT_YELLOW
+echo 'APT update finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get upgrade -y
+echo -e $TEXT_YELLOW
+echo 'APT upgrade finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get install git -y
+sudo apt-get texlive -y
+
+sudo apt-get autoremove -y
+echo -e $TEXT_YELLOW
+echo 'APT auto remove finished...'
+echo -e $TEXT_RESET
+
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo 'Reboot required!'
+    echo -e $TEXT_RESET
+fi
+
+echo "**************************************************************************"
+echo "Installing Mininet-WiFi (which contains Mininet), wireshark, scapy and ryu"
+echo "**************************************************************************"
+sudo apt-get remove mininet -y
+sudo pip uninstall --yes mininet
+sudo rm -r mininet
+echo -e $TEXT_YELLOW
+echo 'Mininet uninstall finished...'
+echo -e $TEXT_RESET
+
+
+git clone https://github.com/intrig-unicamp/mininet-wifi
+cd mininet-wifi/
+cd util/
+sudo ./install.sh -Wlnfv6
+echo -e $TEXT_YELLOW
+echo 'Mininet-WiFi install finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get install wireshark -y 
+sudo pkg-reconfigure wireshark-common
+sudo usermod -aG wireshark $(whoami)
+echo -e $TEXT_YELLOW
+echo 'Wireshark install finished...'
+echo -e $TEXT_RESET
+
+pip install scapy
+pip install PyX
+echo -e $TEXT_YELLOW
+echo 'Scapy install finished...'
+echo -e $TEXT_RESET
+
+git clone https://github.com/faucetsdn/ryu.git
+cd ryu
+pip install .
+echo -e $TEXT_YELLOW
+echo 'Ryu install finished...'
+echo -e $TEXT_RESET
